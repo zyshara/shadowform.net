a4abfc3441532c0337ae86b13a1cb42b8ca5370c
JENNIFER DECKER // @zyshara <jennifer.d.codes@gmail.com>
2025-09-29 01:51:44 -0700

    feat: mobile optimization

--- src/Construction.jsx
+++ src/Construction.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Construction = () => {
   return (
-      <div className="flex h-dvh w-dvw items-center justify-center w-screen font-alkhemikal text-lg text-shadowform-grey">
-        <div className="flex flex-col h-full w-1/2 bg-black justify-center items-center">
-            <img src="/cherry_blossom.png" className="w-10 mb-[14px]"/>
+      <div className="flex h-dvh w-dvw items-center justify-center font-alkhemikal text-5xl text-shadowform-grey flex-col lg:text-lg lg:flex-row">
+        <div className="flex flex-col h-full w-full bg-black justify-center items-center lg:w-1/2">
+            <img src="/cherry_blossom.png" className="w-30 lg:w-10 mb-[14px] white-hard-border"/>
             <hr/>
             <span>under</span>
         </div>
-        <div className="absolute font-8bit text-white text-xs mix-blend-difference"><span>&gt; shadowform.net &lt;</span></div>
-        <div className="flex flex-col h-full w-1/2 bg-white justify-center items-center">
+        <div className="absolute font-8bit text-white text-3xl mix-blend-difference lg:text-xs"><span>&gt; shadowform.net &lt;</span></div>
+        <div className="flex flex-col h-full w-full bg-white justify-center items-center lg:w-1/2">
             <span>construction</span>
             <hr/>
-            <img src="/cherry_blossom.png" className="w-10 mt-[14px]"/>
+            <img src="/cherry_blossom.png" className="w-30 lg:w-10 mt-[14px]"/>
         </div>
       </div>
   )
