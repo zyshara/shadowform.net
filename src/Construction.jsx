0912482035b31dce891f27c81a13cb0b500d81d7
JENNIFER DECKER // @zyshara <jennifer.d.codes@gmail.com>
2025-09-29 01:31:13 -0700

    fix: dynamic height/width viewport

--- src/Construction.jsx
+++ src/Construction.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Construction = () => {
   return (
-      <div className="flex h-screen w-screen items-center justify-center w-screen">
-        <div className="flex flex-col h-screen w-1/2 bg-black text-shadowform-grey justify-center items-center font-alkhemikal">
+      <div className="flex h-dvh w-dvw items-center justify-center w-screen font-alkhemikal text-lg text-shadowform-grey">
+        <div className="flex flex-col h-full w-1/2 bg-black justify-center items-center">
             <img src="/cherry_blossom.png" className="w-10 mb-[14px]"/>
             <hr/>
             <span>under</span>
         </div>
-        <div className="absolute font-8bit text-white text-xs mix-blend-difference">&gt; shadowform.net &lt;</div>
-        <div className="flex flex-col h-screen w-1/2 bg-white text-shadowform-grey justify-center items-center font-alkhemikal">
+        <div className="absolute font-8bit text-white text-xs mix-blend-difference"><span>&gt; shadowform.net &lt;</span></div>
+        <div className="flex flex-col h-full w-1/2 bg-white justify-center items-center">
             <span>construction</span>
             <hr/>
             <img src="/cherry_blossom.png" className="w-10 mt-[14px]"/>
