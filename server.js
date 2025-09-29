9f7fa7a4d1fb65905f1964e9ccdb2c4747884a84
JENNIFER DECKER // @zyshara <jennifer.d.codes@gmail.com>
2025-09-28 22:29:30 -0700

    feat: webpack bundler and react boilerplate

---
+++ server.js
@@ -0,0 +1,44 @@
+// index.js
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000 
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+// SSL Forwarding
+if (process.env.NODE_ENV === "production") {
+   app.use((req, res, next) => {
+      if (req.header("x-forwarded-proto") !== "https") {
+         return res.redirect(`https://${req.header("host")}${req.url}`);
+      }
+      return next();
+   });
+}
+
+// Redirect all requests to 
+app.use((req, res, next) => {
+   if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
+      return next();
+   } else {
+      res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+      res.header("Expires", "-1");
+      res.header("Pragma", "no-cache");
+      return next();
+   }
+});
+
+// Home
+app.get("/", (req, res) => {
+   return res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start the server
+app.listen(port, () => {
+   console.log(`Server listening on http://localhost:${port}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
