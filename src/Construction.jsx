429518f243627ad0f77b016fef680e1d8769c561
JENNIFER DECKER // @zyshara <jennifer.d.codes@gmail.com>
2025-10-11 22:30:05 -0700

    fix: under construction

--- src/Construction.jsx
+++ src/Construction.jsx
@@ -4,15 +4,15 @@ const Construction = () => {
   return (
       <div className="flex h-dvh w-dvw items-center justify-center font-alkhemikal text-5xl text-shadowform-grey flex-col lg:text-lg lg:flex-row">
         <div className="flex flex-col h-full w-full bg-black justify-center items-center lg:w-1/2">
-            <img src="/cherry_blossom.png" className="w-30 lg:w-10 mb-[14px] white-hard-border"/>
-            <hr/>
             <span>under</span>
+            <hr/>
+            <img src="/cherry_blossom.png" className="w-30 lg:w-10 mt-[14px] white-hard-border rotate-180"/>
         </div>
         <div className="absolute font-8bit text-white text-4xl mix-blend-difference lg:text-xs"><span>&gt; shadowform.net &lt;</span></div>
         <div className="flex flex-col h-full w-full bg-white justify-center items-center lg:w-1/2">
-            <span>construction</span>
+            <img src="/cherry_blossom.png" className="w-30 lg:w-10 mb-[14px]"/>
             <hr/>
-            <img src="/cherry_blossom.png" className="w-30 lg:w-10 mt-[14px] grayscale rotate-180"/>
+            <span>construction</span>
         </div>
       </div>
   )
